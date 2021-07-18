e6efe89aebd143bef0b203ac2be4e3dfc6eb0acd
Satoshi Kawabata <kwst.andromeda.ma@gmail.com>
2021-07-18 09:07:11 +0900

    Add children into Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
@@ -19,33 +19,33 @@ const useStyles = makeStyles((theme: Theme) =>
 interface P {
   isShowMenu?: boolean;
   onClickMenu?: () => void;
+  children?: ReactNode;
 }
 
 export const Header = (p: P) => {
   const classes = useStyles();
   const { t } = useLocale();
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          {p.isShowMenu && (
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-              onClick={p.onClickMenu}
-            >
-              <MenuIcon />
-            </IconButton>
-          )}
-          <Link href="/presentations">
-            <Typography variant="h6" className={classes.title}>
-              {t.APP_TITLE}
-            </Typography>
-          </Link>
-        </Toolbar>
-      </AppBar>
-    </>
+    <AppBar position="static">
+      <Toolbar>
+        {p.isShowMenu && (
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={p.onClickMenu}
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
+        <Link href="/presentations">
+          <Typography variant="h6" className={classes.title}>
+            {t.APP_TITLE}
+          </Typography>
+        </Link>
+        {p.children}
+      </Toolbar>
+    </AppBar>
   );
 };
