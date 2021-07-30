4200d6e8bcd98bb3a6a12cfec3818b888aef3a07
Satoshi Kawabata <kwst.andromeda.ma@gmail.com>
2021-07-30 23:09:56 +0900

    Add Sentry #55

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,18 @@ import {
 } from "@material-ui/core";
 import * as gtag from "../src/analytics/gatag";
 import { useRouter } from "next/dist/client/router";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://0ea293edf9e54442b84086875522c78d@o287052.ingest.sentry.io/5886212",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   const theme = createMuiTheme(jaJP);
